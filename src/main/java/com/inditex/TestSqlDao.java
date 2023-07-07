3770663d273735e43b7dc21e45638e76bd1159b6
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-07-07 15:25:36 +0200

    Se realizan mejoras sobre el método copyUserOrders y se agrega driver junto con implementación de getConnection para probar el resultado

--- src/main/java/com/inditex/TestSqlDao.java
+++ src/main/java/com/inditex/TestSqlDao.java
@@ -2,14 +2,8 @@ package com.inditex;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
+import java.sql.*;
+import java.util.*;
 
 /**
  * Mejorar cada uno de los métodos a nivel SQL y código cuando sea necesario
@@ -59,28 +53,53 @@ public class TestSqlDao {
 
     /**
      * Copia todos los pedidos de un usuario a otro
+     * Mejoras:
+     *  - Se cambia el tipo de excepción lanzada a uno más específico (en este caso SQLException)
+     *  - Se cambia a una consulta parametrizada para evitar la inyección de SQL
+     *  - Se trata de optimizar el número de conexiones a base de datos, para ello se almacenan todos los registros
+     *    obtenidos en el select en una lista de Map con los valores para cada columna, una vez tenemos todos los datos
+     *    que necesitamos para los inserts, se van agregando a la consulta del insert los lotes (batch) necesarios,
+     *    ejecutando todos los inserts con una sola conexión
+     *  - Se agregan los campos que faltan para los inserts (ID_TIENDA, ID_USUARIO)
+     *  - Se verifica connection != null
      */
-    public void copyUserOrders(long idUserOri, long idUserDes) throws Exception {
+    public void copyUserOrders(long idUserOri, long idUserDes) throws SQLException {
 
-        String query = String.format("SELECT FECHA, TOTAL, SUBTOTAL, DIRECCION FROM PEDIDOS WHERE ID_USUARIO = %s", idUserOri);
+        String query = "SELECT ID_TIENDA, FECHA, TOTAL, SUBTOTAL, DIRECCION FROM PEDIDOS WHERE ID_USUARIO = ?";
         Connection connection = getConnection();
-        PreparedStatement stmt = connection.prepareStatement(query);
-        ResultSet rs = stmt.executeQuery();
+        if (Objects.isNull(connection)) throw new SQLException("Something was wrong getting data base connection");
+        PreparedStatement selectStatement = connection.prepareStatement(query);
+        selectStatement.setLong(1, idUserOri);
+        ResultSet rs = selectStatement.executeQuery();
 
+        List<Map<String, Object>> rows = new ArrayList<>();
         while (rs.next()) {
-
-            String insert = String.format("INSERT INTO PEDIDOS (FECHA, TOTAL, SUBTOTAL, DIRECCION) VALUES (%s, %s, %s, %s)",
-                    rs.getTimestamp("FECHA"),
-                    rs.getDouble("TOTAL"),
-                    rs.getDouble("SUBTOTAL"),
-                    rs.getString("DIRECCION"));
-
-            Connection connection2 = getConnection();
-            connection2.setAutoCommit(false);
-            PreparedStatement stmt2 = connection2.prepareStatement(insert);
-            stmt2.executeUpdate();
-            connection2.commit();
+            Map<String, Object> row = new HashMap<>();
+            row.put("ID_TIENDA", rs.getLong("ID_TIENDA"));
+            row.put("FECHA", rs.getTimestamp("FECHA"));
+            row.put("TOTAL", rs.getDouble("TOTAL"));
+            row.put("SUBTOTAL", rs.getDouble("SUBTOTAL"));
+            row.put("DIRECCION", rs.getString("DIRECCION"));
+            rows.add(row);
         }
+
+        String insert = "INSERT INTO PEDIDOS (ID_USUARIO, ID_TIENDA, FECHA, TOTAL, SUBTOTAL, DIRECCION) VALUES (?, ?, ?, ?, ?, ?)";
+        PreparedStatement insertStatement = connection.prepareStatement(insert);
+        rows.forEach(info -> {
+            try {
+                insertStatement.setLong(1, idUserDes);
+                insertStatement.setLong(2, (Long) info.get("ID_TIENDA"));
+                insertStatement.setTimestamp(3, (Timestamp) info.get("FECHA"));
+                insertStatement.setDouble(4, (Double) info.get("TOTAL"));
+                insertStatement.setDouble(5, (Double) info.get("SUBTOTAL"));
+                insertStatement.setString(6, (String) info.get("DIRECCION"));
+                insertStatement.addBatch();
+                insertStatement.clearParameters();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        insertStatement.executeBatch();
     }
 
     /**
@@ -118,10 +137,17 @@ public class TestSqlDao {
         }
     }
 
-    private Connection getConnection() {
-
-        // return JDBC connection
-        return null;
+    /**
+     * Se agrega una implementación de getConnection para una base de datos MySQL
+     */
+    private Connection getConnection() throws SQLException {
+        Properties properties = new Properties();
+        properties.put("user", "root");
+        properties.put("password", "root");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost/inditex?serverTimezone=Europe/Madrid&useSSL=false",
+                properties
+        );
     }
 
     @Getter
