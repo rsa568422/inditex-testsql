f59087878fae5187846944500f2a6b447591d736
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-07-07 13:14:23 +0200

    Se realizan mejoras sobre el método getUserMaxOrder

--- src/main/java/com/inditex/TestSqlDao.java
+++ src/main/java/com/inditex/TestSqlDao.java
@@ -1,4 +1,7 @@
 package com.inditex;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -6,6 +9,7 @@ import java.sql.SQLException;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Mejorar cada uno de los métodos a nivel SQL y código cuando sea necesario
@@ -81,26 +85,36 @@ public class TestSqlDao {
 
     /**
      * Obtiene los datos del usuario y pedido con el pedido de mayor importe para la tienda dada
+     * Mejoras:
+     *  - En la implementación actual se obtiene el resultado por los parámetros de la cabecera del procedimiento,
+     *    pero los campos long se pasan por valor y no por referencia, por lo que no serán actualizados. Para solucionar
+     *    este problema se genera una clase auxiliar Order que será la contenedora de los campos a devolver como salida
+     *    de la función, dando como salida un Optional<Order> por si no se encuentran resultados
+     *  - Se eliminan los parámetros de salida de la cabecera
+     *  - Se obtiene la fila con el máximo TOTAL desde la consulta, en lugar de traerse todos los registros
+     *  - Se cambia el tipo de excepción lanzada a uno más específico (en este caso SQLException)
+     *  - Se cambia a una consulta parametrizada para evitar la inyección de SQL
+     *  - Se verifica connection != null
      */
-    public void getUserMaxOrder(long idTienda, long userId, long orderId, String name, String address) throws Exception {
-
-        String query = String.format("SELECT U.ID_USUARIO, P.ID_PEDIDO, P.TOTAL, U.NOMBRE, U.DIRECCION FROM PEDIDOS AS P "
-                + "INNER JOIN USUARIOS AS U ON P.ID_USUARIO = U.ID_USUARIO WHERE P.ID_TIENDA = %", idTienda);
+    public Optional<Order> getUserMaxOrder(long idTienda) throws SQLException {
+        String query = "SELECT U.ID_USUARIO, P.ID_PEDIDO, P.TOTAL, U.NOMBRE, U.DIRECCION "
+                .concat("FROM PEDIDOS AS P INNER JOIN USUARIOS AS U ON P.ID_USUARIO = U.ID_USUARIO ")
+                .concat("WHERE P.ID_TIENDA = ? ORDER BY P.TOTAL DESC LIMIT 1");
         Connection connection = getConnection();
+        if (Objects.isNull(connection)) throw new SQLException("Something was wrong getting data base connection");
         PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setLong(1, idTienda);
         ResultSet rs = stmt.executeQuery();
-        double total = 0;
 
-        while (rs.next()) {
-
-            if (rs.getLong("TOTAL") > total) {
-
-                total = rs.getLong("TOTAL");
-                userId = rs.getInt("ID_USUARIO");
-                orderId = rs.getInt("ID_PEDIDO");
-                name = rs.getString("NOMBRE");
-                address = rs.getString("DIRECCION");
-            }
+        if (rs.next()) {
+            return Optional.of(new Order(
+                    rs.getInt("ID_USUARIO"),
+                    rs.getInt("ID_PEDIDO"),
+                    rs.getString("NOMBRE"),
+                    rs.getString("DIRECCION")
+            ));
+        } else {
+            return Optional.empty();
         }
     }
 
@@ -108,5 +122,14 @@ public class TestSqlDao {
 
         // return JDBC connection
         return null;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Order {
+        private final long userId;
+        private final long orderId;
+        private final String name;
+        private final String address;
     }
 }
